66e8d39b3ed95d85116f58157b04e1415968d01e
jiho9118 <mexocoxon@gmail.com>
2022-11-03 23:50:22 +0900

    장애물 만들기

--- script.js
+++ script.js
@@ -15,11 +15,17 @@ let player = {
   score: 0,
   isplay: false,
 };
+let pipe = {
+  startPos: 0,
+  spaceBetweenRow: 0,
+  spaceBetweenCol: 0,
+  pipeCount: 0,
+};
 
 function start() {
   player.isplay = true;
   player.score = 0;
-  gameArea.innerHTML = "";
+  gameArea.innerHTML = '';
   gameMessage.classList.add('hide');
   startBtn.classList.add('hide');
   let bird = document.createElement('div');
@@ -32,13 +38,68 @@ function start() {
   gameArea.appendChild(bird);
   player.x = bird.offsetLeft;
   player.y = bird.offsetTop;
+
+  pipe.startPos = 0;
+  pipe.spaceBetweenRow = 400;
+  pipe.pipeCount = Math.floor(gameArea.offsetWidth / pipe.spaceBetweenRow);
+
+  for (let i = 0; i < pipe.pipeCount; i++) {
+    makePipe(pipe.startPos + pipe.spaceBetweenRow);
+    pipe.startPos++;
+  }
+
   window.requestAnimationFrame(playGame);
+}
+
+function makePipe(pipePos) {
+  let totalHeight = gameArea.offsetHeight;
+  let totalWidth = gameArea.offsetWidth;
+  let pipeUp = document.createElement('div');
+
+  pipeUp.classList.add('pipe');
+  pipeUp.height = Math.floor(Math.random() * 350);
+  pipeUp.style.height = pipeUp.height + 'px';
+  pipeUp.style.left = totalWidth + pipePos + 'px';
+  pipeUp.x = totalWidth + pipePos;
+  pipeUp.style.top = '0px';
+  pipeUp.style.backgroundColor = 'red';
+  gameArea.appendChild(pipeUp);
+
+  pipe.spaceBetweenCol = Math.floor(Math.random() * 250) + 150;
+
+  let pipeDown = document.createElement('div');
+  pipeDown.classList.add('pipe');
+  pipeDown.style.height =
+    totalHeight - pipeUp.height - pipe.spaceBetweenCol + 'px';
+  pipeDown.style.left = totalWidth - pipePos + 'px';
+  pipeDown.x = totalWidth - pipePos;
+  pipeDown.style.bottom = '0px';
+  pipeDown.style.backgroundColor = 'black';
+  gameArea.appendChild(pipeDown);
+}
+
+function movePipes() {
+  let pipes = document.querySelectorAll('.pipe');
+  let counter = 0;
+  pipes.forEach(function (item) {
+    item.x -= player.speed;
+    item.style.left = item.x + 'px';
+    if (item.x < 0) {
+      item.parentElement.removeChild(item);
+      counter++;
+    }
+  });
+
+  for (let i = 0; i < counter/2; i++) {
+    makePipe(0);
+  }
 }
 
 function playGame() {
   if (player.isplay) {
     let bird = document.querySelector('.bird');
     let wing = document.querySelector('.wing');
+    movePipes();
     let move = false;
 
     if (keys.ArrowLeft && player.x > 0) {
