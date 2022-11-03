3e316ba653a2615201737eb31a059b27b9ec013d
jiho9118 <mexocoxon@gmail.com>
2022-11-03 11:04:57 +0900

    script - 이벤트 등록

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-import "./style.css";
+const score = document.querySelector(".score");
+const startBtn = document.querySelector(".startBtn");
+const gameArea = document.querySelector(".gameArea");
+const gameMessage = document.querySelector('.gameMessage');
+
+startBtn.addEventListener("click", start);
+gameMessage.addEventListener("click", start);
+document.addEventListener("keydown", pressOn);
+document.addEventListener("keyup", pressOff);
+
+function start(){
+  gameMessage.classList.add("hide");
+  startBtn.classList.add("hide");
+}
+
+function pressOn(){
+  console.log("on")
+}
+
+function pressOff(){
+  console.log("off")
+}
